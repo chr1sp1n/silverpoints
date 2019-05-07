560b630e556e0d4bd68444207a2d9893b2690815
Chris <chr1sp1n.dev@gmail.com>
2019-05-07 18:01:54 +0200

    e vai!

---
+++ web/modules/custom/dvl_timer/src/Plugin/Block/DvlTimerBlock.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\dvl_timer\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'DvlTimerBlock' block.
+ *
+ * @Block(
+ *  id = "dvl_timer_block",
+ *  admin_label = @Translation("DVL Timer"),
+ * )
+ */
+class DvlTimerBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    global $base_url;
+    $modulePath = drupal_get_path('module', 'dvl_timer');
+
+
+    $build = [
+      '#theme' => 'dvl_timer',
+      '#data' => [
+      ],
+      '#module_path' => $modulePath,
+      '#base_url' => $base_url,
+      '#attached' => [
+        'library' => [ 'dvl_timer/dvl_timer' ]
+      ],
+    ];
+
+
+    return $build;
+  }
+
+}
